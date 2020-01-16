0813cdbb4d72ff9e05c67ca6737ed0d01ea85379
Mike Koenig <koenigm@seattleu.edu>
2020-01-15 21:32:28 -0800

    Updated Item Default Data

--- Game/Game/Services/DefaultData.cs
+++ Game/Game/Services/DefaultData.cs
@@ -13,12 +13,35 @@ namespace Game.Services
         {
             var datalist = new List<ItemModel>()
             {
-                new ItemModel { Name = "First item", Description="This is an item description." },
-                new ItemModel { Name = "Second item", Description="This is an item description." },
-                new ItemModel { Name = "Third item", Description="This is an item description." },
-                new ItemModel { Name = "Fourth item", Description="This is an item description." },
-                new ItemModel { Name = "Fifth item", Description="This is an item description." },
-                new ItemModel { Name = "Sixth item", Description="This is an item description." }
+                new ItemModel {
+                    Name = "Gold Sword",
+                    Description = "Sword made of Gold, really expensive looking",
+                    ImageURI = "http://www.clker.com/cliparts/e/L/A/m/I/c/sword-md.png",
+                    Range = 0,
+                    Damage = 10,
+                    Value = 10,
+                    Location = ItemLocationEnum.PrimaryHand,
+                    Attribute = AttributeEnum.Defense},
+
+                new ItemModel {
+                    Name = "Strong Shield",
+                    Description = "Enough to hide behind",
+                    ImageURI = "http://www.clipartbest.com/cliparts/4T9/LaR/4T9LaReTE.png",
+                    Range = 0,
+                    Damage = 0,
+                    Value = 10,
+                    Location = ItemLocationEnum.OffHand,
+                    Attribute = AttributeEnum.Defense},
+
+                new ItemModel {
+                    Name = "Bunny Hat",
+                    Description = "Pink hat with fluffy ears",
+                    ImageURI = "http://www.clipartbest.com/cliparts/yik/e9k/yike9kMyT.png",
+                    Range = 0,
+                    Damage = 0,
+                    Value = 10,
+                    Location = ItemLocationEnum.Head,
+                    Attribute = AttributeEnum.Speed},
             };
 
             return datalist;
