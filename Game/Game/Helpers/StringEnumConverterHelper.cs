37b3d6cfaa43d241e713a9cc4f6b9e07e2ce98e6
Mike Koenig <koenigm@seattleu.edu>
2020-01-15 21:22:25 -0800

    String Enum Converter Helper

---
+++ Game/Game/Helpers/StringEnumConverterHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Game.Helpers
+{ 
+    // This converter is used by the Pickers, to change from the picker value to the string value etc.
+    // This allows the convert in the picker to be data bound back and forth the model
+    // The picker requires this because the picker must be a string, but the enum is a value...
+
+    // Converts from a String to the enum value.  Head = 5, would return 5 for the string "Head", and for "Head" will return 5
+    public class StringEnumConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Enum)
+            {
+                return (int)value;
+            }
+
+            if (value is string)
+            {
+                var aa = targetType.GetType();
+                var myReturn = Enum.Parse((targetType), value.ToString());
+                return myReturn;
+            }
+
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int)
+            {
+                var myReturn = Enum.ToObject(targetType, value);
+                return myReturn.ToString();
+            }
+
+            if (value is string)
+            {
+                var aa = targetType.GetType();
+                var myReturn = Enum.Parse((targetType), value.ToString());
+                return myReturn;
+            }
+            return 0;
+        }
+    }
+}
