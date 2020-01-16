8cbd880148d72e7a73364e856dc1eca4cdf7e8d5
Mike Koenig <koenigm@seattleu.edu>
2020-01-15 17:33:29 -0800

    Added Item Service, will hold the web service calls.

---
+++ Game/Game/Services/ItemService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Services
+{
+    static class ItemService
+    {
+        // Return the Default Image URI for the Local Image for an Item.
+        public static string DefaultImageURI = "Item.png";
+    }
+}
