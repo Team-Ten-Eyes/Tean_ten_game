951c0a3703f4be272dcb6361af7b1c8e557eecb4
Mike Koenig <koenigm@seattleu.edu>
2020-01-15 17:19:16 -0800

    Added Helper Folder with AttributeEnumHelper as the first Helper

---
+++ Game/Game/Helpers/AttributeEnumHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Game.Models;
+using System.Linq;
+
+namespace Game.Helpers
+{
+    /// <summary>
+    /// Helper for the Attribute Enum Class
+    /// </summary>
+    static class AttributeEnumHelper
+    {
+        /// <summary>
+        /// Returns a list of strings of the enum for Attribute
+        /// Removes the attributes that are not changable by Items such as Unknown, MaxHealth
+        /// </summary>
+        public static List<string> GetListItem
+        {
+            get
+            {
+                var myList = Enum.GetNames(typeof(AttributeEnum)).ToList();
+                var myReturn = myList;
+                return myReturn;
+            }
+        }
+
+        /// <summary>
+        /// Returns a list of strings of the enum for Attribute
+        /// Removes the unknown
+        /// </summary>
+        public static List<string> GetListCharacter
+        {
+            get
+            {
+                var myList = Enum.GetNames(typeof(AttributeEnum)).ToList();
+                var myReturn = myList;
+                return myReturn;
+            }
+        }
+
+        /// <summary>
+        /// Given the String for an enum, return its value.  That allows for the enums to be numbered 2,4,6 rather than 1,2,3
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static AttributeEnum ConvertStringToEnum(string value)
+        {
+            return (AttributeEnum)Enum.Parse(typeof(AttributeEnum), value);
+        }
+    }
+}
