7fda6235b795343fe3be254d9877bf180037d59d
Mike Koenig <koenigm@seattleu.edu>
2020-01-15 17:14:56 -0800

    Added AttributeEnum

---
+++ Game/Game/Models/Enum/AttributeEnum.cs
@@ -0,0 +1,27 @@
+﻿namespace Game.Models
+{
+    /// <summary>
+    /// The Types of Attributes
+    /// Used by Item to specify what it modifies.
+    /// </summary>
+    public enum AttributeEnum
+    {
+        // Not specified
+        Unknown = 0,    
+
+        // The speed of the character, impacts movement, and initiative
+        Speed = 10,
+
+        // The defense score, to be used for defending against attacks
+        Defense = 12,
+
+        // The Attack score to be used when attacking
+        Attack = 14,
+
+        // Current Health which is always at or below MaxHealth
+        CurrentHealth = 16,
+
+        // The highest value health can go
+        MaxHealth = 18,
+    }
+}
