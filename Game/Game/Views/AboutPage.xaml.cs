57fa544716bb64fbe6cb82e5b7dee1085b6ed555
Mike Koenig <koenigm@seattleu.edu>
2020-01-31 09:12:16 -0800

    About Page switches

--- Game/Game/Views/AboutPage.xaml.cs
+++ Game/Game/Views/AboutPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using Xamarin.Forms;
 
 namespace Game.Views
@@ -15,6 +16,41 @@ namespace Game.Views
         public AboutPage()
         {
             InitializeComponent();
+
+            // Turn off Database Settings Frame
+            DatabaseSettingsFrame.IsVisible = false;
+
+            // Turn off the Debug Frame
+            DebugSettingsFrame.IsVisible = false;
         }
+
+        private void EnableDebugSettings_OnToggled(object sender, ToggledEventArgs e)
+        {
+            // This will change out the DataStore to be the Mock Store if toggled on, or the SQL if off.
+
+            DebugSettingsFrame.IsVisible = (e.Value);
+        }
+
+        private void DatabaseSettingsSwitch_OnToggled(object sender, ToggledEventArgs e)
+        {
+            DatabaseSettingsFrame.IsVisible = (e.Value);
+        }
+
+        private void UseMockDataSourceSwitch_OnToggled(object sender, ToggledEventArgs e)
+        {
+            // This will change out the DataStore to be the Mock Store if toggled on, or the SQL if off.
+            //SetDataSource(e.Value);
+        }
+
+        async void ClearDatabase_Command(object sender, EventArgs e)
+        {
+            var answer = await DisplayAlert("Delete", "Sure you want to Delete All Data, and start over?", "Yes", "No");
+            if (answer)
+            {
+                // Call to the SQL DataStore and have it clear the tables.
+               // SQLDataStore.Instance.InitializeDatabaseNewTables();
+            }
+        }
+
     }
 }
