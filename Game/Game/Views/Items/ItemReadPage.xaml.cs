69094f9031e5260facb5974dd73701485ad37355
Mike Koenig <koenigm@seattleu.edu>
2020-02-03 08:40:48 -0800

    Lint Cleanup ItemRead Page

--- Game/Game/Views/Items/ItemReadPage.xaml.cs
+++ Game/Game/Views/Items/ItemReadPage.xaml.cs
@@ -1,15 +1,10 @@
 ﻿using System.ComponentModel;
 using Xamarin.Forms;
-
-using Game.Models;
 using Game.ViewModels;
 using System;
 
 namespace Game.Views
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
-
     /// <summary>
     /// The Read Page
     /// </summary>
