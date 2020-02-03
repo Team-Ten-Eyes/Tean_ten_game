f66080e099a2629ad0b287a20e158084a18b302d
Mike Koenig <koenigm@seattleu.edu>
2020-02-03 14:19:37 -0800

    Added VillagePage

---
+++ Game/Game/Views/Game/VillagePage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Game.Views
+{
+	/// <summary>
+	/// The Main Game Page
+	/// </summary>
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class VillagePage : ContentPage
+	{
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public VillagePage ()
+		{
+			InitializeComponent ();
+		}
+
+		/// <summary>
+		/// Jump to the Scores
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+        void ScoresButton_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("SU", "Go RedHawks", "OK");
+        }
+
+		/// <summary>
+		/// Jump to the Monsters
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		void MonstersButton_Clicked(object sender, EventArgs e)
+		{
+			DisplayAlert("SU", "Go RedHawks", "OK");
+		}
+
+		/// <summary>
+		/// Jump to the Characters
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		void CharactersButton_Clicked(object sender, EventArgs e)
+		{
+			DisplayAlert("SU", "Go RedHawks", "OK");
+		}
+
+		/// <summary>
+		/// Jump to the Items
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		void ItemsButton_Clicked(object sender, EventArgs e)
+		{
+			Navigation.PushAsync(new ItemIndexPage());
+		}
+	}
+}
