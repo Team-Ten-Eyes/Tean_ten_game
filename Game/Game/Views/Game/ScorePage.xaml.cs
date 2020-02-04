b17974e2f13e16bc82abd49bb69ed01075d31158
Mike Koenig <koenigm@seattleu.edu>
2020-02-03 21:05:13 -0800

    Adding ScorePage and Xaml

---
+++ Game/Game/Views/Game/ScorePage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Game.Views
+{
+	/// <summary>
+	/// The Main Game Page
+	/// </summary>
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ScorePage: ContentPage
+	{
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public ScorePage ()
+		{
+			InitializeComponent ();
+		}
+
+		/// <summary>
+		/// Attack Action
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		async void CloseButton_Clicked(object sender, EventArgs e)
+		{
+			await Navigation.PopModalAsync();
+		}
+	}
+}
