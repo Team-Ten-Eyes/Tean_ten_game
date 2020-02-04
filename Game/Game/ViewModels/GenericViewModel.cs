3193b36c3cd446e57c23d32b0b537742598041fe
Mike Koenig <koenigm@seattleu.edu>
2020-02-03 23:38:31 -0800

    Added GenericViewModel to be used for CRUD pages

---
+++ Game/Game/ViewModels/GenericViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Game.Models;
+
+namespace Game.ViewModels
+{
+    public class GenericViewModel<T> : BaseViewModel<DefaultModel> where T: class
+    {
+        /// <summary>
+        /// The Item Model
+        /// </summary>
+        public T Data { get; set; }
+
+        /// <summary>
+        /// Constructor takes an existing item and sets
+        /// The Title for the page to match the text of data
+        /// The Data to be the passed in data
+        /// </summary>
+        /// <param name="data"></param>
+        public GenericViewModel(T data)
+        {
+            if (data != null)
+            {
+                Title = (data as BaseModel<T>).Name;
+            }
+            Data = data;
+        }
+    }
+}
