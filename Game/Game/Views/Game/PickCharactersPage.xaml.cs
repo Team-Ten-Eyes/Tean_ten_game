0763d76f166ef6bff3670dc08bcb6c88525ad6ed
Mike Koenig <koenigm@seattleu.edu>
2020-02-03 21:04:15 -0800

    Adding PickCharacterPage xaml and cs

---
+++ Game/Game/Views/Game/PickCharactersPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Game.Views
+{
+	/// <summary>
+	/// The Main Game Page
+	/// </summary>
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PickCharactersPage : ContentPage
+	{
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public PickCharactersPage()
+		{
+			InitializeComponent ();
+		}
+
+		/// <summary>
+		/// Jump to the Battle
+		/// 
+		/// Its Modal because don't want user to come back...
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		void BattleButton_Clicked(object sender, EventArgs e)
+		{
+			Navigation.PushModalAsync(new NavigationPage(new PickItemsPage()));
+			Navigation.PopAsync();
+		}
+
+		/// <summary>
+		/// Quit the Battle
+		/// 
+		/// Quitting out
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		void QuitButton_Clicked(object sender, EventArgs e)
+		{
+			Navigation.PopAsync();
+		}
+	}
+}
