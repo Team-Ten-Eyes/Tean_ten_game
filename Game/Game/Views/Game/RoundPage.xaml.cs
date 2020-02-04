6a7a709731614d78e419226d6f1ac247fccc18d5
Mike Koenig <koenigm@seattleu.edu>
2020-02-03 21:05:02 -0800

    Adding RoundPage and xaml

---
+++ Game/Game/Views/Game/RoundPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Game.Views
+{
+	/// <summary>
+	/// The Main Game Page
+	/// </summary>
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class RoundPage: ContentPage
+	{
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public RoundPage ()
+		{
+			InitializeComponent ();
+		}
+
+		/// <summary>
+		/// Start next Round, returning to the battle screen
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		async void NextButton_Clicked(object sender, EventArgs e)
+		{
+			await Navigation.PopModalAsync();
+		}
+	}
+}
