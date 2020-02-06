4148193c0ad01f3ad714ae97ad5d2efb0f79cd0e
Mike Koenig <koenigm@seattleu.edu>
2020-02-06 09:05:29 -0800

    Added ItemIndexViewModelTests class

---
+++ UnitTests/ViewModels/ItemIndexViewModelTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.ViewModels
+{
+    [TestFixture]
+    public class ItemIndexViewModelTests
+    {
+        [Test]
+        public void ItemIndexViewModel_Read_Invalid_ID_Bogus_Should_Fail()
+        {
+            // TODO: Add your test code here
+            var answer = 42;
+            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+        }
+    }
+}
