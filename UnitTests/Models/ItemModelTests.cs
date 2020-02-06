da9e8c744be1e2aabc6bef629d5a4b270b28d6c6
Mike Koenig <koenigm@seattleu.edu>
2020-02-06 09:01:37 -0800

    Added ItemModelTests in the Unit Test Project

---
+++ UnitTests/Models/ItemModelTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class ItemModelTests
+    {
+        [Test]
+        public void TestMethod()
+        {
+
+            var test = new Game.Models.ItemModel();
+            
+            // TODO: Add your test code here
+            var answer = 42;
+            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+        }
+    }
+}
