ef49aebf10b5f4b02605f5d59f36cac59c00d347
Michelle Simoni <michelle.simoni779@gmail.com>
2020-02-10 21:24:37 -0800

    fixed list view by deleting it

--- Game/Game/ViewModels/CharacterViewModel.cs
+++ Game/Game/ViewModels/CharacterViewModel.cs
@@ -2,9 +2,26 @@
 using System.Collections.Generic;
 using System.Text;
 
+using Xamarin.Forms;
+
+using Game.Models;
+using Game.Views;
+
 namespace Game.ViewModels
 {
-    class CharacterViewModel
+    public class CharacterViewModel<T> : BaseViewModel<DefaultModel> where T : class
     {
+        public T Data { get; set; }
+        public CharacterViewModel(T data)
+        {
+            if (data != null)
+            {
+                Title = (data as BaseModel<T>).Name;
+            }
+            Data = data;
+        }
+
+         
+
     }
 }
