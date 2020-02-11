5b74e984b4697199bd798f471ec890543bc97706
Jack Arnold <arnoldj1@seattleu.edu>
2020-02-10 19:58:36 -0800

    Created Attack Option model for use by character's with many attacks

---
+++ Game/Game/Models/AttackOption.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Models
+{
+    class AttackOption
+    {
+        public string Name { get; set; } = "PUNCH!";
+        public int AttackBuff { get; set; } = 1;//Added to attack stats when an attack is made 
+    }
+}
