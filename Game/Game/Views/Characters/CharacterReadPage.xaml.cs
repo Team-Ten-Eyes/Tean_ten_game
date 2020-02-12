0ff6d6c51f332d31fbf9a559b9846ae23b60f067
Jack Arnold <arnoldj1@seattleu.edu>
2020-02-11 21:13:55 -0800

    added button for update to ReadPage toolbar Added button code behind for read page

--- Game/Game/Views/Characters/CharacterReadPage.xaml.cs
+++ Game/Game/Views/Characters/CharacterReadPage.xaml.cs
@@ -23,6 +23,24 @@ namespace Game.Views
 
             BindingContext = this.ViewModel = data;
         }
+
+        async void Update_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new ItemUpdatePage(new ItemViewModel(ViewModel.Data))));
+            await Navigation.PopAsync();
+        }
+
+        /// <summary>
+        /// Calls for Delete
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        //async void Delete_Clicked(object sender, EventArgs e)
+        //{
+        //    await Navigation.PushModalAsync(new NavigationPage(new ItemDeletePage(new ItemViewModel(ViewModel.Data))));
+        //    await Navigation.PopAsync();
+        //}
+
         //public CharacterReadPage()
         //{
         //    InitializeComponent();
