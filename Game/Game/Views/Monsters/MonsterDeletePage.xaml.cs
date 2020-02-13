76e5567638a1fa7d29fd4ff54317f9090f9b3950
William Lathrop <willguy21@gmail.com>
2020-02-12 18:18:14 -0800

    Added Monster Pages - Needs code behind

---
+++ Game/Game/Views/Monsters/MonsterDeletePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Game.Views.Monsters
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MonsterDeletePage : ContentPage
+    {
+        public MonsterDeletePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
