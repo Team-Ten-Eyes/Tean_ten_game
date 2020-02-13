cb2dc7a53f3d502c0b05b0af4658b65df1d7662f
Jack Arnold <arnoldj1@seattleu.edu>
2020-02-12 19:45:46 -0800

    Implemented Monster Delete

--- Game/Game/Views/Monsters/MonsterDeletePage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterDeletePage.xaml.cs
@@ -1,20 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
+using System;
+using System.ComponentModel;
 using Xamarin.Forms;
+using Game.Models;
+using Game.ViewModels;
+
 using Xamarin.Forms.Xaml;
 
-namespace Game.Views.Monsters
+namespace Game.Views
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
+
     public partial class MonsterDeletePage : ContentPage
     {
-        public MonsterDeletePage()
+        GenericViewModel<BaseMonster> viewModel;
+
+
+        public MonsterDeletePage(GenericViewModel<BaseMonster> data)
         {
             InitializeComponent();
+            BindingContext = this.viewModel = data;
+        }
+
+        async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync(false);
+        }
+
+        async void Delete_Clicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Send(this, "Delete", viewModel.Data);
+            await Navigation.PopModalAsync();
         }
     }
 }
