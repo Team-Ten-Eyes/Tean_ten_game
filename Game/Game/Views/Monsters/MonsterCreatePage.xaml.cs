e39c5def815fedde39f15f2d5f3a99f7cac682bc
Jack Arnold <arnoldj1@seattleu.edu>
2020-02-12 19:09:05 -0800

    Minor prep for Monster Create Page

--- Game/Game/Views/Monsters/MonsterCreatePage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterCreatePage.xaml.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Game.Models;
+using Game.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace Game.Views.Monsters
+namespace Game.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MonsterCreatePage : ContentPage
     {
         public MonsterCreatePage()
         {
-            InitializeComponent();
+            InitializeComponent(); 
         }
     }
 }
