99c268a92161a9f3228590cedb9ea74aa7686145
Michelle Simoni <michelle.simoni779@gmail.com>
2020-02-13 18:59:23 -0800

    added comments to the character create page

--- Game/Game/Views/Monsters/MonsterCreatePage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterCreatePage.xaml.cs
@@ -23,17 +23,32 @@ namespace Game.Views
             BindingContext = this.ViewModel = data;
         }
 
+
+        /// <summary>
+        /// will save the monster the user just created the list
+        /// </summary>
+        /// <returns></returns>
         public async void OnSaveButtonClicked(object sender, EventArgs e)
         {
             MessagingCenter.Send(this, "Create", ViewModel.Data);
             await Navigation.PopModalAsync();
         }
 
+        /// <summary>
+        /// the cancle button with back out of the create page and 
+        /// Direct the user back to the list page
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         async void CancelButtonClicked(object sender, EventArgs e)
         {
             await Navigation.PopModalAsync();
         }
 
+        /// <summary>
+        /// this suppresses the back button on android becuase it is a modal page
+        /// </summary>
+        /// <returns></returns>
         protected override bool OnBackButtonPressed()
         {
             return true;
