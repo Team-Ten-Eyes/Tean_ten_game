45cf2079d9753f1216ab8c386f554a1943ca1b85
William Lathrop <willguy21@gmail.com>
2020-02-18 17:37:29 -0800

    Added Char Limit To Char Update

--- Game/Game/Views/Characters/CharacterUpdatePage.xaml.cs
+++ Game/Game/Views/Characters/CharacterUpdatePage.xaml.cs
@@ -25,8 +25,12 @@ namespace Game.Views
 
         public async void OnSaveButtonClicked(object sender, EventArgs e)
         {
-            MessagingCenter.Send(this, "Update", ViewModel.Data);
-            await Navigation.PopModalAsync();
+            if (ViewModel.Data.Name.Length > 12)
+                await DisplayAlert("Name Too Long", "Must Be Less Than 13 Chars", "OK");
+            else {
+                MessagingCenter.Send(this, "Update", ViewModel.Data);
+                await Navigation.PopModalAsync();
+            }
         }
 
         protected override bool OnBackButtonPressed()
