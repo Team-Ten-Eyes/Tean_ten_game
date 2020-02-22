50e73c61cf99fccdd316046186d1613319f0a6c2
Jack Arnold <arnoldj1@seattleu.edu>
2020-02-22 11:14:21 -0800

    adding player model for char and monster to inherit from

---
+++ Game/Game/Models/PlayerModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Models
+{
+    class PlayerModel : BaseModel<PlayerModel>
+    {
+        public uint Level { get; set; } = 1;
+    }
+}
