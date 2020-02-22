be36ed8afec2bad6fdb1b11e0afd2d77c9811c7b
Michelle Simoni <michelle.simoni779@gmail.com>
2020-02-22 12:27:08 -0800

    chaged the namespaces in the added files and also added a roundEnum that will help determine if the round ended

---
+++ Game/Game/Models/Enum/RoundEnum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Models
+{
+    public enum RoundEnum
+    {
+        // Invalid State
+        Unknown = 0,
+
+        // Next Turn, monsters and Characters still alive
+        NextTurn = 1,
+
+        // New Round, Monsters dead
+        NewRound = 2,
+
+        // Game Over, characters dead
+        GameOver = 3,
+    }
+}
