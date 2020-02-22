c11a1f89f5b99e420d63a8fbe6a9d6207209f9b7
Michelle Simoni <michelle.simoni779@gmail.com>
2020-02-22 12:15:49 -0800

    added all the battle engine files

--- Game/Game/Engine/BaseEngine.cs
+++ Game/Game/Engine/BaseEngine.cs
@@ -2,9 +2,16 @@
 using System.Collections.Generic;
 using System.Text;
 
+using System.Collections.Generic;
+using Game.Models;
+
 namespace Game.Engine
-{
+{   
+    /// <summary>
+    /// Holds the Data Structures for the Battle engine
+    /// </summary>
     class BaseEngine
     {
+      
     }
 }
