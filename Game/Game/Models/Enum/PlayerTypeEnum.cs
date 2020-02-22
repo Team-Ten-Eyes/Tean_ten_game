e0160a60e86ccf3c7316a99041393a2fd0e0bede
Michelle Simoni <michelle.simoni779@gmail.com>
2020-02-22 12:23:27 -0800

    adding a player type enum that will help in battle turns

---
+++ Game/Game/Models/Enum/PlayerTypeEnum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Models.Enum
+{
+
+        /// <summary>
+        /// The player in the round, Monster or Character
+        /// </summary>
+        public enum PlayerTypeEnum
+        {
+            // Not Known
+            Unknown = 0,
+
+            // The Character
+            Character = 1,
+
+            // The Monster
+            Monster = 2,
+        }
+    
+}
