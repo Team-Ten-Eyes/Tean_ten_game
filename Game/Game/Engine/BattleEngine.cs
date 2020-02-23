525f59e914d5a897468b922de7a64a3462e57e35
Michelle Simoni <michelle.simoni779@gmail.com>
2020-02-22 17:02:49 -0800

    Commited battle Engine

--- Game/Game/Engine/BattleEngine.cs
+++ Game/Game/Engine/BattleEngine.cs
@@ -1,10 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
+using Game.Models;
 
 namespace Game.Engine
 {
-    public class BattleEngine
+    /// <summary>
+    /// Battle Engine for the Game
+    /// </summary>
+    public class BattleEngine : RoundEngine
     {
+        // Track if the Battle is Running or Not
+        public bool BattleRunning = false;
+
+        /// <summary>
+        /// Add the charcter to the character list
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public bool PopulateCharacterList(BaseCharacter data)
+        {
+            CharacterList.Add(new PlayerInfoModel(data));
+
+            return true;
+        }
+
+        /// <summary>
+        /// Start the Battle
+        /// </summary>
+        /// <param name="isAutoBattle"></param>
+        /// <returns></returns>
+        public bool StartBattle(bool isAutoBattle)
+        {
+            // Reset the Score so it is fresh
+            BattleScore = new ScoreModel
+            {
+                AutoBattle = isAutoBattle
+            };
+
+            BattleRunning = true;
+
+            NewRound();
+
+            return true;
+        }
+
+        /// <summary>
+        /// End the Battle
+        /// </summary>
+        /// <returns></returns>
+        public bool EndBattle()
+        {
+            BattleRunning = false;
+
+            return true;
+        }
     }
 }
