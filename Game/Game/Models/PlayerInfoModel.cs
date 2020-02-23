4e179e5177f475b055fd6699a91910c8fcc1e5b2
Michelle Simoni <michelle.simoni779@gmail.com>
2020-02-22 16:48:29 -0800

    added all the code for playerinfoModel

--- Game/Game/Models/PlayerInfoModel.cs
+++ Game/Game/Models/PlayerInfoModel.cs
@@ -13,5 +13,91 @@ namespace Game.Models
     /// </summary>
     class PlayerInfoModel : PlayerModel<PlayerInfoModel>
     {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public PlayerInfoModel() { }
+
+        /// <summary>
+        /// Copy from one PlayerInfoModel into Another
+        /// </summary>
+        /// <param name="data"></param>
+        public PlayerInfoModel(PlayerInfoModel data)
+        {
+            PlayerType = data.PlayerType;
+            Guid = data.Guid;
+            Alive = data.Alive;
+            Experience = data.ExpereinceTotal;
+            Level = data.Level;
+            Name = data.Name;
+            Description = data.Description;
+            Speed = data.GetSpeed();
+            ImageURI = data.ImageURI;
+            CurrHealth = data.GetCurrentHealthTotal;
+            MaxHealth = data.GetMaxHealthTotal;
+
+            // Set the strings for the items
+            Head = data.Head;
+            Feet = data.Feet;
+            Necklass = data.Necklass;
+            RightFinger = data.RightFinger;
+            LeftFinger = data.LeftFinger;
+            Feet = data.Feet;
+        }
+
+        /// <summary>
+        /// Create PlayerInfoModel from character
+        /// </summary>
+        /// <param name="data"></param>
+        public PlayerInfoModel(BaseCharacter data)
+        {
+            PlayerType = data.PlayerType;
+            Guid = data.Guid;
+            Alive = data.Alive;
+            Experience = data.ExpereinceTotal;
+            Level = data.Level;
+            Name = data.Name;
+            Description = data.Description;
+            Speed = data.GetSpeed();
+            ImageURI = data.ImageURI;
+            CurrHealth = data.GetCurrentHealthTotal;
+            MaxHealth = data.GetMaxHealthTotal;
+
+            // Set the strings for the items
+            Head = data.Head;
+            Feet = data.Feet;
+            Necklass = data.Necklass;
+            RightFinger = data.RightFinger;
+            LeftFinger = data.LeftFinger;
+            Feet = data.Feet;
+        }
+
+        /// <summary>
+        /// Crate PlayerInfoModel from Monster
+        /// </summary>
+        /// <param name="data"></param>
+        public PlayerInfoModel(BaseMonster data)
+        {
+            PlayerType = data.PlayerType;
+            Guid = data.Guid;
+            Alive = data.Alive;
+            Experience = data.ExpereinceTotal;
+            Level = data.Level;
+            Name = data.Name;
+            Description = data.Description;
+            Speed = data.GetSpeed();
+            ImageURI = data.ImageURI;
+            CurrHealth = data.GetCurrentHealthTotal;
+            MaxHealth = data.GetMaxHealthTotal;
+
+            // Set the strings for the items
+            Head = data.Head;
+            Feet = data.Feet;
+            Necklass = data.Necklass;
+            RightFinger = data.RightFinger;
+            LeftFinger = data.LeftFinger;
+            Feet = data.Feet;
+        }
     }
 }
+
