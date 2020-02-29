f1c202f7e89f18ff0ab35add23cfc831419922fe
Michelle Simoni <michelle.simoni779@gmail.com>
2020-02-29 12:48:46 -0800

    Connected Monsters lurking page to mikes generic battle screen so that we can edit and see the changes

--- Game/Game/Views/Battle/BattlePage.xaml.cs
+++ Game/Game/Views/Battle/BattlePage.xaml.cs
@@ -92,5 +92,10 @@ namespace Game.Views
 				await Navigation.PopModalAsync();
 			}
 		}
+
+		async void Go_to_monsters_lurking(object sender, EventArgs e)
+		{
+			await Navigation.PushAsync(new MonstersInTheNextBattle());
+		}
 	}
 }
