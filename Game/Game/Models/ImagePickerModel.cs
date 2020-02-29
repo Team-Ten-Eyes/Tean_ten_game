f46762b06253d3a295c9e97a85177537c18f8233
Michelle Simoni <michelle.simoni779@gmail.com>
2020-02-29 11:51:23 -0800

    added an item picker model class and default data for an item picker list

---
+++ Game/Game/Models/ImagePickerModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Models
+{
+    /// <summary>
+    /// this is an image class that would help when creating
+    /// the image picker
+    /// </summary>
+    public class ImagePickerModel
+    {
+        /// <summary>
+        /// This is the image url
+        /// </summary>
+        public string Url { get; set; } = null;
+    }
+}
--- Game/Game/Views/Characters/CharacterCreatePage.xaml.cs
+++ Game/Game/Views/Characters/CharacterCreatePage.xaml.cs
@@ -28,7 +28,7 @@ namespace Game.Views
 
             CharacterTypePicker.SelectedItem = data.Data.Attribute.ToString();
 
-            itemPickerModel ImageSelect = new itemPickerModel();
+          
            
 
         }
