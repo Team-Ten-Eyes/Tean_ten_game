51ec6e49b3ce636301edc9d9b70f3bb94901d7ff
Michelle Simoni <michelle.simoni779@gmail.com>
2020-03-03 16:42:26 -0800

    binded our item database list to the character equiped page

--- Game/Game/Views/Battle/CharacterEquiped.xaml.cs
+++ Game/Game/Views/Battle/CharacterEquiped.xaml.cs
@@ -15,13 +15,24 @@ namespace Game.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CharacterEquiped : ContentPage
     {
+
+        //a gineric view model of the character in the party
         readonly GenericViewModel<BaseCharacter> ViewModel;
 
+        // This uses the Instance so it can be shared with other Battle Pages as needed
+        //using this to equip items from the database
+        public BattleEngineViewModel EngineViewModel = BattleEngineViewModel.Instance;
 
         public CharacterEquiped(GenericViewModel<BaseCharacter> data)
         {
             InitializeComponent();
             BindingContext = this.ViewModel = data;
+            ItemListView.ItemsSource = EngineViewModel.DatabaseItemList;
+        }
+
+        public void On_items_selected(object sender, SelectedItemChangedEventArgs args)
+        {
+            DisplayAlert("Attack!!!", "Attack !!!", "OK");
         }
     }
 }
