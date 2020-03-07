624fa8d8b3179b04fcfae0c11f8f8c6bf9e2e096
William Lathrop <willguy21@gmail.com>
2020-03-06 19:23:07 -0800

    Commented Hackathon Test

--- Game/Game/Engine/TurnEngine.cs
+++ Game/Game/Engine/TurnEngine.cs
@@ -134,7 +134,7 @@ namespace Game.Engine
         /// Pick the Monster to Attack
         /// </summary>
         /// <returns></returns>
-        public PlayerInfoModel SelectMonsterToAttack()
+        public PlayerInfoModel SelectMonsterToAttack(string)
         {
             if (PlayerList == null)
             {
@@ -186,13 +186,13 @@ namespace Game.Engine
 
             // Hackathon
             // Hackathon Scenario 2, Bob alwasys misses
-            if (Attacker.Name.Equals("Bob"))
-            {
-                BattleMessagesModel.HitStatus = HitStatusEnum.Miss;
-                BattleMessagesModel.TurnMessage = "Bob always Misses";
-                Debug.WriteLine(BattleMessagesModel.TurnMessage);
-                return true;
-            }
+            //if (Attacker.Name.Equals("Bob"))
+            //{
+            //    BattleMessagesModel.HitStatus = HitStatusEnum.Miss;
+            //    BattleMessagesModel.TurnMessage = "Bob always Misses";
+            //    Debug.WriteLine(BattleMessagesModel.TurnMessage);
+            //    return true;
+            //}
 
             switch (BattleMessagesModel.HitStatus)
             {
