1e73c9145ba7fe9f0e4bccafed0bcac7d98d78bb
Michelle Simoni <michelle.simoni779@gmail.com>
2020-03-09 20:09:25 -0700

    potions class and addition of the enum type

---
+++ Game/Game/Models/Enum/PotionsEnumClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Models
+{
+    
+        /// <summary>
+        /// This is the enum for the potions model that would determine 
+        /// potions functionality 
+        /// </summary>
+        public enum PotionsEnum
+        {
+            // Not specified
+            Unknown = 0,
+
+            Health = 1,
+
+            Mana = 2,
+
+
+        }
+
+        /// <summary>
+        /// Friendly strings for the Enum Class
+        /// </summary>
+        public static class PotionsEnumExtensions
+        {
+            /// <summary>
+            /// Display a string for the Enums
+            /// </summary>
+            /// <param name="value"></param>
+            /// <returns></returns>
+            public static string ToMessage(this PotionsEnum value)
+            {
+                //default string
+                var Message = "unknown";
+
+                switch (value)
+                {
+                    case PotionsEnum.Health:
+                        Message = "Health";
+                        break;
+                    case PotionsEnum.Mana:
+                        Message = "Mana";
+                        break;
+                }
+                return Message;
+            }
+        }
+    
+    
+}
---
+++ Game/Game/Models/PotionsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Models
+{
+    class PotionsModel
+    {
+        //this is the potions type
+        public PotionsEnum PotionType {  get; set; } = PotionsEnum.Mana;
+
+        //this it the amount of health or mana the potion would give to a character
+        public uint potionType { get; set; } = 1;
+    }
+}
