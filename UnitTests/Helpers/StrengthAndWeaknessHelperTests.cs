4ed315c8e8fe688ce4629726bb021eed49652ae2
Michelle Simoni <michelle.simoni779@gmail.com>
2020-03-12 17:36:19 -0700

    creativity expected strengths pushed

--- UnitTests/Helpers/StrengthAndWeaknessHelperTests.cs
+++ UnitTests/Helpers/StrengthAndWeaknessHelperTests.cs
@@ -12,6 +12,7 @@ namespace UnitTests.Helpers
     [TestFixture]
     class StrengthAndWeaknessHelperTests
     {
+      
         [Test]
         public void SrengthAndWeaknessHelper_get_expected_strengths_bravery_should_pass()
         {
@@ -23,7 +24,7 @@ namespace UnitTests.Helpers
 
             //getting expected strengths
             var monsterType = Enum.GetNames(typeof(MonsterTypeEnum)).ToList();
-            var BraveStrengths = monsterType.Where(a =>
+            var Expected = monsterType.Where(a =>
                        a.ToString() != MonsterTypeEnum.Paranoia.ToString() &&
                        a.ToString() != MonsterTypeEnum.Unknown.ToString() &&
                        a.ToString() != MonsterTypeEnum.Anger.ToString() &&
@@ -35,7 +36,34 @@ namespace UnitTests.Helpers
 
             //assert
             //should be the same
-            Assert.AreEqual(BraveStrengths, result);
+            Assert.AreEqual(Expected, result);
+        }
+
+        [Test]
+        public void SrengthAndWeaknessHelper_get_expected_strengths_Creativity_should_pass()
+        {
+            //arrange
+            //creating a Creative character class
+            BaseCharacter character = new BaseCharacter();
+            character.Attribute = CharacterTypeEnum.Creativity;
+
+            var monsterType = Enum.GetNames(typeof(MonsterTypeEnum)).ToList();
+            //getting expected strengths
+            var Expected= monsterType.Where(a =>
+             a.ToString() != MonsterTypeEnum.Anxiety.ToString() &&
+             a.ToString() != MonsterTypeEnum.Unknown.ToString() &&
+             a.ToString() != MonsterTypeEnum.Depression.ToString() &&
+             a.ToString() != MonsterTypeEnum.Insanity.ToString() &&
+             a.ToString() != MonsterTypeEnum.BurnOut.ToString()
+                  ).ToList();
+
+            //Act
+            var result = StrengthWeaknessHelper.getCharacterStrengths(character.Attribute.ToString());
+
+            //assert
+            //should be the same
+            Assert.AreEqual(Expected, result);
+
         }
 
 
