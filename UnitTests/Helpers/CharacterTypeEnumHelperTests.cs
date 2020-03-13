003f02bd782999fbc2a32ae46d871ee7733faba9
Michelle Simoni <michelle.simoni779@gmail.com>
2020-03-12 19:17:57 -0700

    changed the class for enum helper to be public and created a test for createCharacterType

---
+++ UnitTests/Helpers/CharacterTypeEnumHelperTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+using Game.Models;
+using Game.Helpers;
+
+
+namespace UnitTests.Helpers
+{
+    [TestFixture]
+    class CharacterTypeEnumHelperTests
+    {
+        [Test]
+        public void characterTypeListAll_should_have_unkown_should_pass()
+        {
+            //arrange
+            string unknown = "Unknown";
+            bool hasUnknown = false;
+
+            //act
+            var result = CharacterTypeEnumHelper.GetListCharacterTypeALL;
+
+            foreach (string type in result)
+            {
+                if (type == unknown)
+                {
+                    hasUnknown = true;
+                    break;
+                }
+            }
+            //Assert
+            Assert.AreEqual(true, hasUnknown);
+        }
+    }
+}
