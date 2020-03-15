7c1043f7d9f47c51822477da63708ce56aea13c5
Michelle Simoni <michelle.simoni779@gmail.com>
2020-03-15 14:45:28 -0700

    added all tests for player info model

---
+++ UnitTests/Models/PlayerInfoModelTests.cs
@@ -0,0 +1,100 @@
+﻿using NUnit.Framework;
+
+using Game.Models;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class PlayerInfoModelTests
+    {
+        [Test]
+        public void PlayerInfoModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+            var data = new PlayerInfoModel();
+
+            // Act
+            var result = new PlayerInfoModel(data);
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void PlayerInfoModel_Constructor_Character_Default_Should_Pass()
+        {
+            // Arrange
+            var data = new BaseCharacter();
+
+            // Act
+            var result = new PlayerInfoModel(data);
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void PlayerInfoModel_Constructor_Monster_Default_Should_Pass()
+        {
+            // Arrange
+            var data = new BaseMonster();
+
+            // Act
+            var result = new PlayerInfoModel(data);
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void PlayerInfoModel_Constructor_Character_Fighter_Default_Should_Pass()
+        {
+            // Arrange
+            var data = new BaseCharacter { Attribute = CharacterTypeEnum.Cunning };
+
+            // Act
+            var result = new PlayerInfoModel(data);
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void PlayerInfoModel_Constructor_Character_Cleric_Default_Should_Pass()
+        {
+            // Arrange
+            var data = new BaseCharacter { Attribute = CharacterTypeEnum.Creativity };
+
+            // Act
+            var result = new PlayerInfoModel(data);
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void PlayerInfoModel_Constructor_Character_Unknown_Default_Should_Pass()
+        {
+            // Arrange
+            var data = new BaseCharacter { Attribute = CharacterTypeEnum.Bravery};
+
+            // Act
+            var result = new PlayerInfoModel(data);
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
