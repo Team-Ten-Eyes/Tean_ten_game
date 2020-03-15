e17fdbf7a81076b55f385c6ae2c759ddc17fb833
Michelle Simoni <michelle.simoni779@gmail.com>
2020-03-15 12:43:17 -0700

    test to see if default character atributes all return as expected

--- UnitTests/Models/BaseCharacterTests.cs
+++ UnitTests/Models/BaseCharacterTests.cs
@@ -66,5 +66,52 @@ namespace UnitTests.Models
             Assert.IsNotNull(result.Mana);
             Assert.IsNotNull(result.Attribute);
         }
+
+        [Test]
+        public void BaseCharacter_Set_Default_Should_Pass()
+        {
+            //ArDefense
+
+            //Act
+            var result = new BaseCharacter();
+            result.Attack = 6;
+            result.Defense = 7;
+            result.Speed = 8;
+
+            //Reset
+
+            //Assert
+            Assert.AreEqual(6, result.Attack);
+            Assert.AreEqual(7, result.Defense);
+            Assert.AreEqual(8, result.Speed);
+
+            Assert.IsNotNull(result.Id);
+            Assert.AreEqual(result.Id, result.Guid);
+
+            Assert.AreEqual("Michelle.png", result.ImageURI);
+            Assert.AreEqual(PlayerTypeEnum.Character, result.PlayerType);
+
+
+            Assert.AreEqual(true, result.Alive);
+            Assert.AreEqual(0, result.Order);
+            Assert.AreEqual(0, result.ListOrder);
+            Assert.AreEqual(1, result.Level);
+            Assert.AreEqual(299, result.ExperienceRemaining);
+            Assert.AreEqual(1, result.CurrHealth);
+            Assert.AreEqual(1, result.Mana);
+            Assert.AreEqual(1, result.MaxMana);
+            Assert.AreEqual(1, result.MaxHealth);
+            Assert.AreEqual(0, result.Experience);
+
+            Assert.AreEqual(null, result.Head);
+            Assert.AreEqual(null, result.Feet);
+            Assert.AreEqual(null, result.Necklass);
+            Assert.AreEqual(null, result.PrimaryHand);
+            Assert.AreEqual(null, result.OffHand);
+            Assert.AreEqual(null, result.RightFinger);
+            Assert.AreEqual(null, result.LeftFinger);
+
+            Assert.AreEqual(CharacterTypeEnum.Bravery, result.Attribute);
+        }
     }
 }
