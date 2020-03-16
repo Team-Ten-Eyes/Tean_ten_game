469bffb970e83aa64c5dc2084e15220715dcf487
Michelle Simoni <michelle.simoni779@gmail.com>
2020-03-15 21:16:00 -0700

    Added An Image picker tests

---
+++ UnitTests/Models/imagePickerModelTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+using Game.Models;
+using Game.ViewModels;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    class imagePickerModelTests
+    {
+        [Test]
+        public void ImagePickerModel_Default_Is_Null_Should_Pass()
+        {
+            //Arrange
+
+            //Act
+            ImagePickerModel test = new ImagePickerModel();
+
+            //Assert
+            Assert.AreEqual(null, test.Url);
+            
+        }
+    }
+}
