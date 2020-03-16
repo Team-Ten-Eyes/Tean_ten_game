4820d8c4d71e01c579c48b0b077b4005abd0fa51
Michelle Simoni <michelle.simoni779@gmail.com>
2020-03-15 21:27:00 -0700

    added a default expected test for potions model

---
+++ UnitTests/Models/PotionsModelTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+using Game.Models;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    class PotionsModelTests
+    {
+        [Test]
+        public void potionsModel_expected_default_potion_should_pass()
+        {
+            //Arrange
+
+            //Act
+            var result = new PotionsModel();
+            //Assert
+            Assert.AreEqual(10, result.Addition);
+            Assert.AreEqual(PotionsEnum.Health, result.potionType);
+            Assert.AreEqual("Health.png", result.ImageURI);
+        }
+    }
+}
