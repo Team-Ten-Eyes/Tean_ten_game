aaacc04c0f04af0d440e87d7cc8d60b8f6f10e46
Michelle Simoni <michelle.simoni779@gmail.com>
2020-03-18 15:52:20 -0700

    added a function in character type Enum and added tests for character type

--- UnitTests/Models/Enums/CharacterTypeEnumTests.cs
+++ UnitTests/Models/Enums/CharacterTypeEnumTests.cs
@@ -1,10 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NUnit.Framework;
 
-namespace UnitTests.Models.Enums
+using Game.Models;
+
+namespace UnitTests.Models
 {
+    [TestFixture]
     class CharacterTypeEnumTests
     {
+        [Test]
+        public void CharacterTypeEnumExtensionsTests_Unknown_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = CharacterTypeEnum.Unknown.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Player", result);
+        }
+
+        [Test]
+        public void CharacterJobEnumExtensionsTests_Bravery_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = CharacterTypeEnum.Bravery.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Bravery", result);
+        }
+
+        [Test]
+        public void CharacterJobEnumExtensionsTests_Creativity_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = CharacterTypeEnum.Creativity.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Creativity", result);
+        }
+
+        [Test]
+        public void CharacterJobEnumExtensionsTests_Cunning_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = CharacterTypeEnum.Cunning.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Cunning", result);
+        }
     }
 }
