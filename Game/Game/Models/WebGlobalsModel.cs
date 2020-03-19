b3a4c3d2e62572e14da48dcbc547077b33649d02
William Lathrop <willguy21@gmail.com>
2020-03-19 16:02:05 -0700

    Added Files For Database Connections

--- Game/Game/Models/WebGlobalsModel.cs
+++ Game/Game/Models/WebGlobalsModel.cs
@@ -6,7 +6,7 @@
     public static class WebGlobalsModel
     {
         // URL for the web site
-        public const string WebSiteAPIURL = "https://itemgetpost.azurewebsites.net/API/";
+        public static string WebSiteAPIURL = "https://itemgetpost.azurewebsites.net/API/";
 
         // Error code value used in the json returns
         public const int ErrorResultCode = 1;
